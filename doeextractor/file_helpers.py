5a37a01a62c86219f79b115c12000a706244bdd4
Aldrin Navarro <aldrinnavarro16@gmail.com>
2022-05-28 10:57:40 +0800

    Fix textractor UnsupportedDocumentException
    1. Split PDF to PNG by pages so that it may not exceed 5MB (AWS Textract
       Bytes limit)
    2. Fix Bytes input to use the converted PNG format
    
    Signed-off-by: Aldrin Navarro <aldrinnavarro16@gmail.com>

--- doeextractor/file_helpers.py
+++ doeextractor/file_helpers.py
@@ -56,27 +56,48 @@ def is_file_already_analyzed(file_path):
         return first_result is not None, first_result
 
 
-def convert_pdf_to_png(file_path):
+def convert_pdf_to_png(file_path, merge_pages=False) -> Path:
     """
     Convert a PDF file to PNG.
     """
-    output_file_path = (
-        Path(__file__).parent.parent / "output" / (file_path.stem + ".png")
-    )
-    if output_file_path.exists():
-        print("File already exists")
-        return output_file_path.absolute()
+    if merge_pages:
+        output_file_path = (
+            Path(__file__).parent.parent / "output" / (file_path.stem + ".png")
+        )
+        if output_file_path.exists():
+            print("Output file already exists.")
+            return output_file_path.absolute()
+    else:
+        output_file_dir = Path(__file__).parent.parent / "output" / file_path.stem
+        has_files = False
+        try:
+            has_files = len(list(output_file_dir.iterdir())) > 0
+        except FileNotFoundError:
+            pass
+        if has_files:
+            print("Output directory already exists and is not empty.")
+            return output_file_dir.absolute()
+        else:
+            output_file_dir.mkdir(parents=True, exist_ok=True)
 
     with tempfile.TemporaryDirectory() as path:
         images_from_path = convert_from_path(file_path, output_folder=path)
-        new_width = images_from_path[0].size[0]
-        new_heigth = images_from_path[0].size[1] * len(images_from_path)
-        new_image = Image.new("RGB", (new_width, new_heigth), color=(255, 255, 255))
-        for i, image in enumerate(images_from_path):
-            new_image.paste(image, (0, i * image.size[1]))
 
-        # TODO make output directory a variable
-        output_dir = Path(__file__).parent.parent / "output"
-        new_image.save(output_file_path, format="PNG")
-        print("Saved to " + str(output_file_path))
-        return output_file_path.absolute()
+        if merge_pages:
+            new_width = images_from_path[0].size[0]
+            new_heigth = images_from_path[0].size[1] * len(images_from_path)
+            new_image = Image.new("RGB", (new_width, new_heigth), color=(255, 255, 255))
+            for i, image in enumerate(images_from_path):
+                new_image.paste(image, (0, i * image.size[1]))
+            # TODO make output directory a variable
+            output_dir = Path(__file__).parent.parent / "output"
+            new_image.save(output_file_path, format="PNG")
+            print("Saved to " + str(output_file_path))
+            return output_file_path.absolute()
+        else:
+            for i, image in enumerate(images_from_path):
+                new_image = Image.new("RGB", image.size, color=(255, 255, 255))
+                new_image.paste(image)
+                new_image.save(output_file_dir / (str(i) + ".png"), format="PNG")
+            print(f"Saved {len(images_from_path)} pages to {str(output_file_dir)}")
+            return output_file_dir.absolute()
